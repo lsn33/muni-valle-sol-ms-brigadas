c3281701844eb23dbbf46f1f8066fd1b56deaa4f
lsn33 <lucassoto3333@gmail.com>
2026-06-12 17:04:03 -0400

    Base proyecto ms-brigadas con Spring Initializr

---
+++ src/main/java/cl/municipalidad/msbrigadas/MsBrigadasApplication.java
@@ -0,0 +1,13 @@
+package cl.municipalidad.msbrigadas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBrigadasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBrigadasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/municipalidad/msbrigadas/MsBrigadasApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.municipalidad.msbrigadas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBrigadasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
